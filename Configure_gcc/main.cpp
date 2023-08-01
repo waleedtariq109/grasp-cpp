0be68269ff5d2d49621eee575546894151722fcd
Waleed Tariq <waleedtariq109@outlook.com>
2023-08-01 22:50:19 +0500

    INITIAL COMMIT

---
+++ Configure_gcc/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num1;
+  int num2;
+  cout << "Enter a 1st value: ";
+  cin >> num1;
+  cout << "Enter a 2nd value: ";
+  cin >> num2;
+  cout << "The addition of Value 1 and Value 2 is " << (num1 + num2) << endl;
+}
