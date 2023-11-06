ee21b74b191f0544f8672d54de0f93d8b5fd50f0
Waleed Tariq <waleedtariq109@outlook.com>
2023-11-06 10:13:33 +0500

    .

---
+++ 4-integers-and-decimal/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    /**
+     * Three ways to declare variable in C++
+     * 1: Braced Initialization
+     * 2: Functional Initialization
+     * 3: Assignment Initialization
+    */
+
+    // Braced Initialization
+    int number; // Initialize with some garbage value: Some compiler may give warning
+    int number1 {}; // Initialize with 0 as a default value
+
+    return 0;
+};
