bea8a6fb038f7598aad30e27cfc5669380595453
Waleed Tariq <waleedtariq109@outlook.com>
2023-11-07 20:57:13 +0500

    decimal and integer

--- 1-variables-and-dataTypes/main.cpp
+++ 1-variables-and-dataTypes/main.cpp
@@ -2,36 +2,85 @@
 
 int main()
 {
-    // Represent Number in different Number system
+    // Integers in C++ & different ways of initialize a variable
+
+    // What is Integers in C++
     /*
-        You need to define number 15 in following Number system
-        1: Decimal
-        2: Binary
-        3: Octal
-        4: Hexadecimal
+        1: You can declare a integer type variable using int keyword
+        2: Integers stores whole number not decimal point number like 10.52
+        3: Typically integers occupy 4 bytes or more in memory
     */
 
+    // Different ways of initialize a variable
     /*
-         1: The total of 8 bits represent 1 byte
-         2: Signed and unsigned are terms used to describe the interpretation
-            of binary data (bits) in a computer system. They are particularly
-            relevant in the context of integer data types, and they determine
-            whether the binary representation is used to represent positive and negative
-            numbers (signed) or only non-negative numbers (unsigned).
+        1: Asignment Variable Initialization
+        2: Braces Variable Initialization
+        3: Functional Variable Initialization
+     */
 
+    // What is Variable.
+    /*
+        1: Variable is simply the way to store data in memory.
+        2: We can think in a way like we store some data in let's say 'data' variable
+           so that data was stored in memory and we can refer to that piece of memory
+           using the variable name 'data'.
+        3: Variable name should start with character after that we can use the integer and
+           apart from that we can't use any special character except _ in variable name.
+        4: Variable are case-sensitive.
     */
 
-    // This feature only comes in C++14 or Greater
+    // Variable syntax
+    /*
+          1: You need to define the data type
+          2: After data type you need to give it a name
+          3: After that you need to give the value
 
-    int numInDecimal = 15;    // In Decimal
-    int numInOctal = 017;     // In Octal
-    int numInHexa = 0xf;      // In Hexadecimal
-    int numInBinary = 0b1111; // In binary
 
-    std::cout << "Decimal Representation : " << numInDecimal << std::endl;
-    std::cout << "Octal Representation : " << numInOctal << std::endl;
-    std::cout << "Hexadecimal Representation : " << numInHexa << std::endl;
-    std::cout << "Binary Representation : " << numInBinary << std::endl;
+          Syntax Example: typename variable_name {value}
+    */
+
+    std::cout << "------Braced Initialization------" << std::endl;
+
+    int elephant_count; // May contain some garbage Value . WARNING
+    int lion_count{};   // Initialize with 0
+    int cat_count{10};  // Initialize with 10
+    int dog_count{15};  // Initialize with 10
+    int total_animals{cat_count + dog_count + lion_count};
+    // int narrowing_conersion_with_braces {10.9} // This will give error
+
+    std::cout << "You may see some garbage value: " << elephant_count << std::endl;
+    std::cout << "Lion Count: " << lion_count << std::endl;
+    std::cout << "Cat Count: " << cat_count << std::endl;
+    std::cout << "Dog Count: " << dog_count << std::endl;
+    std::cout << "Total Animals: " << total_animals << std::endl;
+
+    std::cout << "------Functional Initialization------" << std::endl;
+
+    int apple_count(10);                            // Contains 10
+    int orange_count(20);                           // Contains 20
+    int total_fruits(apple_count + orange_count);   // Contains apple_count + orange_count
+    int narrowing_conersion_with_functional(10.56); // Narrow the value may lost some data and less safer the brace initialization. NOT RECOMMENDED
+
+    std::cout << "Apple Count: " << apple_count << std::endl;
+    std::cout << "Orange Count: " << apple_count << std::endl;
+    std::cout << "Total Fruits: " << total_fruits << std::endl;
+    std::cout << "Narrowing conersion with Functional Initialization: " << narrowing_conersion_with_functional << std::endl;
+
+    std::cout << "------Assignment Initialization------" << std::endl;
+
+    int bike_count = 10;                             // Contains 10
+    int car_count = 20;                              // Contains 20
+    int total_vehicle = bike_count + car_count;      // Contains bike_count + car_count
+    int narrowing_conersion_with_assignment = 10.56; // Narrow the value may lost some data and less safer the brace initialization. NOT RECOMMENDED
+
+    std::cout << "Bike Count: " << bike_count << std::endl;
+    std::cout << "Car Count: " << bike_count << std::endl;
+    std::cout << "Total Vehicle: " << total_vehicle << std::endl;
+    std::cout << "Narrowing conersion with Assignment Initialization: " << narrowing_conersion_with_assignment << std::endl;
+
+    std::cout << "------TIP: We can also the variable memory size using sizeof()------" << std::endl;
+    std::cout << "Size of int: " << sizeof(int) << std::endl;
+    std::cout << "Size of bike_count variable: " << bike_count << std::endl;
 
     return 0;
-};
+}
