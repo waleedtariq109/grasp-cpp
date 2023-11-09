a07f8245143137fa4f7c4e572cd1a54ea33a9f06
Waleed Tariq <waleedtariq109@outlook.com>
2023-11-09 21:42:04 +0500

    auto keyword

--- 1-variables-and-dataTypes/main.cpp
+++ 1-variables-and-dataTypes/main.cpp
@@ -2,35 +2,46 @@
 
 int main()
 {
-    // Characters and Texts
+    // Auto keyword
     /*
-        We used char keyword as a type for storing a character
-        Char data type takes 1 bytes in memory
-        In C++ we can also assign a valid ASCII code to a char variable,
-        and it will store a corresponsing character in memory.
-
-        https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+        The "auto" keyword is used to instruct the compiler to deduce the type.
+        If we create a variable using the "auto" keyword, the compiler will attempt
+        to find the type of the value you assign to the variable and set the
+        variable to that type.
     */
 
-    char character1{'a'};
-    char character2{'r'};
-    char character3{'r'};
-    char character4{'o'};
-    char character5{'w'};
+    auto var1{10};
+    auto var3{10.98};
+    auto var2{9.19f};
+    auto var4{14.76l};
+    auto var5{'a'};
 
-    std::cout << character1 << std::endl;
-    std::cout << character2 << std::endl;
-    std::cout << character3 << std::endl;
-    std::cout << character4 << std::endl;
-    std::cout << character5 << std::endl;
+    // Using integers modifiers
+    auto var6{120u};  // unsigned integer
+    auto var7{98ul};  // unsigned long integer
+    auto var8{887ll}; // long long integer
 
-    // char data type takes 1 byte in memory: 2^8 = 256 different values (0 ~ 255)
+    // Print
+    std::cout << "The value of var1: " << var1 << std::endl;
+    std::cout << "The value of var2: " << var2 << std::endl;
+    std::cout << "The value of var3: " << var3 << std::endl;
+    std::cout << "The value of var4: " << var4 << std::endl;
+    std::cout << "The value of var5: " << var5 << std::endl;
+    std::cout << "The value of var6: " << var6 << std::endl;
+    std::cout << "The value of var7: " << var7 << std::endl;
+    std::cout << "The value of var8: " << var8 << std::endl;
+
     std::cout << std::endl;
 
-    // We can also store a ASCII value in char data type
-    char ascii_value = 65; // ASCII character code for 'A'
-    std::cout << "Value: " << ascii_value << std::endl;
-    std::cout << "Value after casting: " << static_cast<int>(ascii_value) << std::endl;
+    // Size
+    std::cout << "The size of var1: " << sizeof(var1) << std::endl;
+    std::cout << "The size of var2: " << sizeof(var2) << std::endl;
+    std::cout << "The size of var3: " << sizeof(var3) << std::endl;
+    std::cout << "The size of var4: " << sizeof(var4) << std::endl;
+    std::cout << "The size of var5: " << sizeof(var5) << std::endl;
+    std::cout << "The size of var6: " << sizeof(var6) << std::endl;
+    std::cout << "The size of var7: " << sizeof(var7) << std::endl;
+    std::cout << "The size of var8: " << sizeof(var8) << std::endl;
 
     return 0;
-}
+};
