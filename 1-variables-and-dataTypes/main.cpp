ffbe244a172a47b8da14f486d030fa3022600ddc
Waleed Tariq <waleedtariq109@outlook.com>
2023-11-11 19:15:55 +0500

    table

--- 1-variables-and-dataTypes/main.cpp
+++ 1-variables-and-dataTypes/main.cpp
@@ -2,5 +2,16 @@
 
 int main() {
     std::cout << "Hello" << std::endl;
+    unsigned int whichTable, tableStartsForm, tableEndsWith;
+    std::cout << "Which table you want to see? " << std::endl;
+    std::cin >> whichTable;
+    std::cout << "From where you want to starts the " << whichTable << " table?" << std::endl;
+    std::cin >> tableStartsForm;
+    std::cout << "Where you want to end the " << whichTable << " table?" << std::endl;
+    std::cin >> tableEndsWith;
+
+    for(unsigned int i = tableStartsForm; i <= tableEndsWith; ++i) {
+        std::cout << whichTable << " X " << i << " = " << whichTable * i << std::endl;
+    }
     return 0;
 }
