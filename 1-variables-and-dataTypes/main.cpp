6a885e57d13ce91439ef631f07ad3f1ff4374a42
Waleed Tariq <waleedtariq109@outlook.com>
2023-11-12 23:57:40 +0500

    assigmnets

--- 1-variables-and-dataTypes/main.cpp
+++ 1-variables-and-dataTypes/main.cpp
@@ -2,5 +2,17 @@
 
 int main() {
     // Assignments
+    int var1{10};
+    std::cout << "Var 1 (brace): " << var1 << std::endl;
+    var1 = 20;
+    std::cout << "Var 1 (Assignment): " << var1 << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "----------------" << std::endl;
+
+    double var2{99.88};
+    std::cout << "Var 1 (brace): " << var2 << std::endl;
+    var2 = 20.45;
+    std::cout << "Var 1 (Assignment): " << var2 << std::endl;
     return 0;
 }
