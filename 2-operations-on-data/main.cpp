8f3d29891e3e9d3aefe37a39b26c45e8c4f2557d
Waleed Tariq <waleedtariq109@outlook.com>
2023-11-13 23:44:11 +0500

    .

--- 2-operations-on-data/main.cpp
+++ 2-operations-on-data/main.cpp
@@ -1,49 +1,6 @@
 #include <iostream>
 
 int main() {
-    // Basic Operations
-    /*
-     * 1: Addition
-     * 2: Multiplication
-     * 3: Subtraction
-     * 4: Division
-     * 5: Modulus
-     * */
-
-    int result;
-
-    // Addition
-    int number1 = 20;
-    int number2 = 30;
-    result = number1 + number2;
-
-    std::cout << "The Addition is: " << result << std::endl;
-
-    // Subtraction
-    int number3 = 20;
-    int number4 = 30;
-    result = number3 - number4;
-    std::cout << "The Subtraction is: " << result << std::endl;
-
-    // Multiplication
-    int number5 = 3;
-    int number6 = 10;
-    result = number5 * number6;
-    std::cout << "The Multiplication is: " << result << std::endl;
-
-
-    // Division
-    int number7 = 31;
-    int number8 = 3;
-    result = number7 / number8;
-    std::cout << "The Division is: " << result << std::endl;
-
-    // Modulus
-    int number9 = 31;
-    int number10 = 3;
-    result = number9 % number10;
-    std::cout << "The Subtraction is: " << result << std::endl;
-
-
+    // Precedence and associativity
     return 0;
 }
