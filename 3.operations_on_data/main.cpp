def7d20258de4aa144783923fdf073303a934120
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-08-11 00:41:19 +0500

    compound assignment

---
+++ 3.operations_on_data/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int value {38};
+
+  std::cout << value << std::endl;
+
+  // value = value + 12;
+  value += 12;
+
+  std::cout << value << std::endl;
+
+  return 0;
+}
