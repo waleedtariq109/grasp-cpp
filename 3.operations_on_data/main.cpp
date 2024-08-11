b6aa232a570ccc64f623b829ddeb2902604544aa
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-08-11 17:24:47 +0500

    number limits

--- 3.operations_on_data/main.cpp
+++ 3.operations_on_data/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
-
+  // Number limits
+  std::cout << "The range of short is from" << std::numeric_limits<short>::min() << " and the maximim range of short is " << std::numeric_limits<short>::max();
   return 0;
 }
