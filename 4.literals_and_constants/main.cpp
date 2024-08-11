127f8c7b20805e3fd8a4f6ebe2a7716bd51d363e
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-08-12 00:14:44 +0500

    constinit

--- 4.literals_and_constants/main.cpp
+++ 4.literals_and_constants/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+const int val1{33};
+constexpr int val2{34};
+int val3{35};
+
+constinit int age = 88;
+const constinit int age1{val1};
+constinit int age2{age1};
+
+const constinit double weight{23.4};
+
 int main()
 {
+  // constinit
   return 0;
 }
