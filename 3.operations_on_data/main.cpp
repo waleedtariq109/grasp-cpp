ee9def18a547a841bf1d8652ae4a46de9ca7f171
Waleed Tariq <waleed.tariq@sukuktech.com>
2024-08-11 17:04:40 +0500

    std cout and std endl

--- 3.operations_on_data/main.cpp
+++ 3.operations_on_data/main.cpp
@@ -2,6 +2,22 @@
 
 int main()
 {
+  // Output formatting
+
+  std::cout << "Hello";
+  std::cout << "World";
+
+  std::cout << std::endl;
+
+  std::cout << "------------" << std::endl;
+
+  std::cout << "Hello" << std::endl;
+  std::cout << "World" << std::endl;
+
+  std::cout << std::endl;
+
+  std::cout << "Hello\n";
+  std::cout << "World\n";
 
   return 0;
 }
