4281db79963746bdfe151eb4a78723b365392508
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-16 01:23:01 +0500

    integers to binary

--- 6.bitwise_operator/main.cpp
+++ 6.bitwise_operator/main.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-#include <bitset>
 
 int main()
 {
-  // Printing intergers in binary
-
-  unsigned int short data{0b111111110};
-
-  std::cout << "Data(dec): " << std::dec << data << std::endl;
-  std::cout << "Data(hex): " << std::showbase << std::hex << data << std::endl;
-  std::cout << "Data(bin): " << std::bitset<16>(data) << std::endl;
-
+  // Bitwise Operator
   return 0;
 }
