a4b529ad7f54e2ec1faceb6e1437f3be677f011b
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-18 02:27:39 +0500

    switch with initializer

--- 8.control-flow/main.cpp
+++ 8.control-flow/main.cpp
@@ -2,4 +2,27 @@
 
 int main()
 {
+  const int Eraser{1};
+  const int Pen{2};
+  const int Marker{3};
+
+  const int tool = Marker;
+
+  switch (int strength{20}; tool)
+  {
+  case Eraser:
+    std::cout << "Your Tool is Eraser " << strength << std::endl;
+    break;
+  case Pen:
+    std::cout << "Your Tool is Pen " << strength << std::endl;
+    break;
+  case Marker:
+    std::cout << "Your Tool is marker " << strength << std::endl;
+    break;
+  default:
+    std::cout << "You have no matching tool" << std::endl;
+    break;
+  }
+
+  return 0;
 }
