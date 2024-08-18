5914c7e01cb555e54fe3ca854c9e1c70af1d1fb7
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-19 02:38:56 +0500

    loops

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -2,6 +2,27 @@
 
 int main()
 {
-  // Loops
+  // For Loops
+
+  // Noob way
+  /*
+  std::cout << "C++: The GOAT Language" << std::endl;
+  std::cout << "C++: The GOAT Language" << std::endl;
+  std::cout << "C++: The GOAT Language" << std::endl;
+  std::cout << "C++: The GOAT Language" << std::endl;
+  std::cout << "C++: The GOAT Language" << std::endl;
+  std::cout << "C++: The GOAT Language" << std::endl;
+  */
+
+  // Pro Way
+  for (unsigned int i{0}; i < 10; ++i)
+  {
+    std::cout << "C++: The GOAT Language" << std::endl;
+  }
+
+  for (size_t i{0}; i < 10; ++i)
+  {
+    std::cout << "Hello C++" << std::endl;
+  }
   return 0;
 }
