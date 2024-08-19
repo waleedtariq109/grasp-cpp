807f9c9154d151b61dc30e29428ccc274549e435
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-20 01:13:05 +0500

    while loop

---
+++ 9.loops/while-loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  // While loop
+  const size_t COUNT{10};
+  size_t i{0};
+  while (i < COUNT)
+  {
+    std::cout << "C++: The GOAT" << std::endl;
+    ++i;
+  }
+  return 0;
+}
