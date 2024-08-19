b7767e36545be3e690e781863d1b062b96eaefcd
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-20 00:00:46 +0500

    range based loop

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
+  // Range based for loops
+  int bag_of_values[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  // The value variable will assigned a value of bag_of_values in each iteration
+  for (int value : bag_of_values)
+  {
+    std::cout << "Value: " << value << std::endl;
+  }
+
   return 0;
 }
