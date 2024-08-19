1e63126162f94c8635dff357eb286d808af70c24
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-19 22:23:56 +0500

    setup

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -2,22 +2,5 @@
 
 int main()
 {
-  // Comma Operator
-  /**
-   * The comma operator combines two or more expression into one single expression
-   * The return value of the operation is the value of it right operand
-   */
-
-  int increment{5};
-  int number1{10};
-  int number2{20};
-  int number3{25};
-
-  int result = (number1 *= ++increment, number2 - (++increment), number3 += ++increment);
-  std::cout << number1 << std::endl;
-  std::cout << number2 << std::endl;
-  std::cout << number3 << std::endl;
-  std::cout << result << std::endl;
-
   return 0;
 }
