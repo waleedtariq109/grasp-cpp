c67929905feda7bd72b62ad2138b3218562ce9cd
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-20 00:35:21 +0500

    setup

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -2,39 +2,6 @@
 
 int main()
 {
-  // Range based for loops
-  int bag_of_values[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // This is a collection of integers
-
-  // The value variable will assigned a value of bag_of_values in each iteration
-  for (int value : bag_of_values)
-  {
-    std::cout << "Value: " << value << std::endl;
-  }
-
-  std::cout << std::endl;
-
-  std::cout << "---------------" << std::endl;
-
-  std::cout << std::endl;
-
-  // We can even give the value in range based loop litrally
-  for (int value : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-  {
-    std::cout << "Value: " << value << std::endl;
-  }
-
-  std::cout << std::endl;
-
-  std::cout << "---------------" << std::endl;
-
-  std::cout << std::endl;
-
-  // Auto type deduction
-
-  for (auto value : bag_of_values)
-  {
-    std::cout << "Value: " << value << std::endl;
-  }
-
+  // While loop
   return 0;
 }
