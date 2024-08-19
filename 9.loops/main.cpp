db74158d5ef82a2fcafed14c941ad76e7880d618
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-20 01:32:52 +0500

    do while loop

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -3,5 +3,14 @@
 int main()
 {
   // Do While loops
+  const size_t COUNT{10};
+  size_t i{40}; // Iterator
+
+  do
+  {
+    std::cout << "C++: THE GOAT LANGUAGE" << std::endl;
+    ++i; // 41
+  } while (i < COUNT);
+
   return 0;
 }
