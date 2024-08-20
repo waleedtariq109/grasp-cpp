e4b3e09e78d23ee0c01169eca825c6cc5d43268e
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-21 02:28:22 +0500

    break

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -3,5 +3,15 @@
 int main()
 {
   // Break and Continue
+
+  // Terminating the loop
+  std::cout << "The break statement" << std::endl;
+  for (size_t i{0}; i < 20; ++i)
+  {
+    std::cout << "index: " << i << std::endl;
+    // Terminate or stop the loop if i == 6
+    if (i == 6)
+      break;
+  }
   return 0;
 }
