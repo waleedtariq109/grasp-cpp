7eae955b36f518aedcff1c7a74503853e08a8938
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-21 02:06:30 +0500

    nested for loops

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
+  // Nested loop
+  std::cout << "Tabular Data Visualization with nested for loops" << std::endl;
+
+  const size_t ROWS{12};
+  const size_t COLS{3};
+
+  for (size_t row{0}; row < ROWS; ++row)
+  {
+    for (size_t cols{0}; cols < COLS; ++cols)
+    {
+      std::cout << "( row " << std::setw(2) << row << ", cols " << std::setw(2) << cols << " )";
+    }
+    std::cout << std::endl;
+  }
+
   return 0;
 }
