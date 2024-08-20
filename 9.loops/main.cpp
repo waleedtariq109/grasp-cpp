f301111719bf87d0cf697ea16b2ddf4155d90744
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-21 02:14:24 +0500

    nested while loops

--- 9.loops/main.cpp
+++ 9.loops/main.cpp
@@ -9,14 +9,28 @@ int main()
   const size_t ROWS{12};
   const size_t COLS{3};
 
-  for (size_t row{0}; row < ROWS; ++row)
+  size_t row{0};
+  size_t cols{0};
+  while (row < ROWS)
   {
-    for (size_t cols{0}; cols < COLS; ++cols)
+    while (cols < COLS)
     {
       std::cout << "( row " << std::setw(2) << row << ", cols " << std::setw(2) << cols << " )";
+      ++cols;
     }
     std::cout << std::endl;
+    cols = 0;
+    ++row;
   }
+
+  // for (size_t row{0}; row < ROWS; ++row)
+  // {
+  //   for (size_t cols{0}; cols < COLS; ++cols)
+  //   {
+  //     std::cout << "( row " << std::setw(2) << row << ", cols " << std::setw(2) << cols << " )";
+  //   }
+  //   std::cout << std::endl;
+  // }
 
   return 0;
 }
