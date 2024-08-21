eb2efd8c37047c0d54f059a95c72b0e9dd64265f
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-22 01:05:15 +0500

    declare and read value from array

--- 10.arrays/main.cpp
+++ 10.arrays/main.cpp
@@ -3,5 +3,13 @@
 int main()
 {
   std::cout << "Arrays" << std::endl;
+  std::cout << std::endl;
+
+  // Decalre an array of ints
+  int scores[10]; // The 10 we specified is the size of an array
+
+  // Read the Data
+  std::cout << "Score[0]: " << scores[0] << std::endl;
+
   return 0;
 }
