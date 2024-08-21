0c20b019dd0ab8e6144c90e1d82ea4146e0efe0d
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-22 01:23:07 +0500

    array

--- 10.arrays/main.cpp
+++ 10.arrays/main.cpp
@@ -14,10 +14,39 @@ int main()
   // Read data with loop
   for (size_t i{0}; i < 10; ++i)
   {
-    std::cout << "scores[" << i << "]: " << i << scores[i] << std::endl;
+    std::cout << "scores[" << i << "]: " << scores[i] << std::endl;
   }
 
   std::cout << std::endl;
+
+  // Write data
+  scores[0] = 20;
+  scores[1] = 21;
+  scores[2] = 22;
+
+  for (size_t i{0}; i < 10; ++i)
+  {
+    std::cout << "scores[" << i << "]: " << scores[i] << std::endl;
+  }
+
+  std::cout << std::endl;
+
+  // Write value using loop
+  for (size_t i{0}; i < 10; ++i)
+  {
+    scores[i] = i * 2;
+    std::cout << "scores[" << i << "]: " << scores[i] << std::endl;
+  }
+
+  // Declare and initialize data at the same time
+  std::cout << std::endl;
+  std::cout << "Declare and initialize data at the same time" << std::endl;
+
+  double salaries[5]{99.2, 12.8, 7.9, 82.3, 67.99};
+  for (size_t i{0}; i < 5; ++i)
+  {
+    std::cout << "salaries[" << i << "]: " << salaries[i] << std::endl;
+  }
 
   return 0;
 }
