6756ea29adf4052933657be9c27e62fb30192896
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-22 01:12:59 +0500

    read data with loop

--- 10.arrays/main.cpp
+++ 10.arrays/main.cpp
@@ -2,14 +2,22 @@
 
 int main()
 {
-  std::cout << "Arrays" << std::endl;
-  std::cout << std::endl;
-
   // Decalre an array of ints
   int scores[10]; // The 10 we specified is the size of an array
 
   // Read the Data
   std::cout << "Score[0]: " << scores[0] << std::endl;
+  std::cout << "Score[1]: " << scores[1] << std::endl;
+
+  std::cout << std::endl;
+
+  // Read data with loop
+  for (size_t i{0}; i < 10; ++i)
+  {
+    std::cout << "scores[" << i << "]: " << i << scores[i] << std::endl;
+  }
+
+  std::cout << std::endl;
 
   return 0;
 }
