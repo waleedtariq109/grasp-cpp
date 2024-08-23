29441c19e80d5d8f77da58ff63590bf5f9702b43
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-24 03:59:06 +0500

    declare and print character array

--- 10.arrays/main.cpp
+++ 10.arrays/main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+  // Declare Character Arrays
+  char message[5]{'h', 'e', 'l', 'l', 'o'};
+
+  // Print message
+  std::cout << "Message: ";
+  for (auto c : message)
+  {
+    std::cout << c;
+  }
+  std::cout << std::endl;
+
   return 0;
 }
