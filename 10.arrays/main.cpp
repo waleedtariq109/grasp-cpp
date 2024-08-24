3b83b783b35f6d2a1fde24e743e7c8315bd5ca90
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-24 23:20:35 +0500

    std::rand

--- 10.arrays/main.cpp
+++ 10.arrays/main.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
+  // Generating Random number
+
+  /**
+   * The std::rand() will return a integer between 0 ~ RAND_MAX
+   * The value of RAND_MAX is depending system to system
+   * To check the value of RAND_MAX we can `cout` the RAND_MAX
+   *
+   * The RAND_MAX is already defines in standard C++ linrary
+   */
+
+  int random_number = std::rand(); // 0 ~ RAND_MAX
+  std::cout << "Random Number: " << random_number << std::endl;
+
+  random_number = std::rand();
+  std::cout << "Random Number: " << random_number << std::endl;
+
+  std::cout << std::endl;
+
   return 0;
 }
