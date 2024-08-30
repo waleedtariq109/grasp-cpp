ac8c7f23d4a8cef1c4878f09b41b2264c98b3e23
Waleed Tariq <waleedtariq109@outlook.com>
2024-08-31 01:12:46 +0500

    setup

--- 11.pointers/pointers-char-array.cpp
+++ 11.pointers/pointers-char-array.cpp
@@ -5,19 +5,6 @@ int main() {
   // srand() has to run once per program run
   std::srand(std::time(0));  // Seed
 
-  /*
-  char prediction0[]{ "a lot of kinds running in the backyard!" };
-  char prediction1[]{ "a lot of empty beer bootles on your work table." };
-  char prediction2[]{ "you Partying too much with kids wearing weird clothes." };
-  char prediction3[]{ "you running away from something really scary" };
-  char prediction4[]{ "clouds gathering in the sky and an army standing ready for war" };
-  char prediction5[]{ "dogs running around in a deserted empty city" };
-  char prediction6[]{ "a lot of cars stuck in a terrible traffic jam" };
-  char prediction7[]{ "you sitting in the dark typing lots of lines of code on your dirty computer" };
-  char prediction8[]{ "you yelling at your boss. And oh no! You get fired!" };
-  char prediction9[]{ "you laughing your lungs out. I've never seen this before." };
-  */
-
   char predictions[][90]{
       "a lot of kinds running in the backyard!",
       "a lot of empty beer bootles on your work table.",
