66d9876899ac27b12e61e80bff54984ee0c64113
Waleed Tariq <waleedtariq109@outlook.com>
2024-09-03 00:03:03 +0500

    raw variable

--- 11.pointers/const-ptr-and-ptr-to-const.cpp
+++ 11.pointers/const-ptr-and-ptr-to-const.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
 int main() {
+  // Non const vaiable
+  std::cout << std::endl;
+  std::cout << "Raw variable that can be modified" << std::endl;
+
+  int number{5};  // Not a const and value can be changes
+  std::cout << "number: " << number << std::endl;
+  std::cout << "&number: " << &number << std::endl;
+
+  // Modify the value
+  number = 15;
+  number += 7;
+
+  // Print number and memory address
+  std::cout << "number: " << number << std::endl;
+  std::cout << "&number: " << &number << std::endl;
+
   return 0;
 }
