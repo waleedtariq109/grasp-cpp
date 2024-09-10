c59d8866a2430f8bffdc11bebb5033bcd221c723
Waleed Tariq <waleedtariq109@outlook.com>
2024-09-10 19:48:54 +0500

    SFS-6893

--- 11.pointers/array-of-const-pointer-const-char.cpp
+++ 11.pointers/array-of-const-pointer-const-char.cpp
@@ -1,24 +1,5 @@
 #include <iostream>
 
 int main() {
-  const char *students[]{
-      "Alice",
-      "Bob",
-      "Charlie",
-      "David",
-      "Eve",
-      "Frank",
-      "Grace",
-      "Hannah",
-  };
-
-  std::cout << "Printing out the students" << std::endl;
-  for (const char *student : students) {
-    std::cout << student << std::endl;
-  }
-
-  const char *new_student{"Bob"};
-  std::cout << *new_student << std::endl;
-
   return 0;
 }
