159634cc009054a5ae254caceb30d4e1cbeb690c
Waleed Tariq <waleedtariq109@outlook.com>
2024-09-15 04:16:45 +0500

    .

--- 11.pointers/array-of-const-pointer-const-char.cpp
+++ 11.pointers/array-of-const-pointer-const-char.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
 int main() {
+  const char *students[]{
+      "Student1",
+      "Student2",
+      "Student3",
+      "Student4",
+      "Student5",
+  };
+
+  // Print students
+  std::cout << std::endl;
+  for (const char *student : students) {
+    std::cout << student << std::endl;
+  }
   return 0;
 }
