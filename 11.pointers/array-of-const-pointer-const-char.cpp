8184b1bf9f3677e2d9ce879166e60bfa290e58de
Waleed Tariq <waleedtariq109@outlook.com>
2024-09-15 04:19:53 +0500

    .

--- 11.pointers/array-of-const-pointer-const-char.cpp
+++ 11.pointers/array-of-const-pointer-const-char.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 
 int main() {
-  const char *students[]{
-      "Student1",
-      "Student2",
-      "Student3",
-      "Student4",
-      "Student5",
+  const char* const students[]{
+      "Daniel Gray",
+      "Tuna Philbe",
+      "The Rock",
+      "Kumar Singh",
+      "Sam Ali",
+      "Dave Mahoro",
   };
 
-  // Print students
+  // Print out the students
   std::cout << std::endl;
-  for (const char *student : students) {
+  std::cout << "Printing out the students : " << std::endl;
+  for (const char* student : students) {
     std::cout << student << std::endl;
   }
+
+  // *students[0] = 'K';
+
+  // Can swap for new student though.
+  const char* new_student{"Bob the Great"};
+  // students[0] = new_student; // Compiler error
+
+  // Print out the students
+  std::cout << std::endl;
+  std::cout << "Printing out the students : " << std::endl;
+  for (const char* student : students) {
+    std::cout << student << std::endl;
+  }
+
   return 0;
 }
