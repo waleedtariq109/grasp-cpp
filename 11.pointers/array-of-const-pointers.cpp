750221c30de93e5de55023aa9801100fcd47204f
Waleed Tariq <waleedtariq109@outlook.com>
2024-09-22 00:37:09 +0500

    setup

---
+++ 11.pointers/array-of-const-pointers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+  const char *const students[]{
+      "Daniel Gray",
+      "Tuna Philbe",
+      "The Rock",
+      "Kumar Singh",
+      "Sam Ali",
+      "Dave Mahoro",
+  };
+
+  std::cout << std::endl;
+  std::cout << "Printing out the students" << std::endl;
+  for (const char *student : students) {
+    std::cout << student << std::endl;
+  }
+
+  // *students[0] 'K'; //-> Compiler Error
+
+  const char *new_student{"Bob the great"};
+  // students[0] = new_student;
+
+  std::cout << std::endl;
+  std::cout << "Printing out the students" << std::endl;
+  for (const char *student : students) {
+    std::cout << student << std::endl;
+  }
+
+  return 0;
+}
