6e3a2566a20b1c98ff447c3bf7deb85115397df9
Waleed Tariq <waleedtariq109@outlook.com>
2024-09-22 20:06:41 +0500

    setup

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 
 int main() {
-  int value{99};
-  int val1{1};
-
-  int result{value + val1};
-
-  std::cout << "The result of value + val1 is: " << result << std::endl;
-
   return 0;
 }
