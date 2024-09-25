c22be904c985b8475e5079af772c17f63e0dc6c3
Waleed Tariq <waleedtariq109@outlook.com>
2024-09-25 19:44:01 +0500

    moving array data

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -28,5 +28,29 @@ int main() {
   }
   std::cout << std::endl;
 
+  // Sawaping Array Data through pointer
+
+  std::cout << "Swapping Array Data through pointers" << std::endl;
+
+  int arr3[5]{1, 2, 3, 4, 5};
+  int arr4[5]{6, 7, 8, 9, 10};
+
+  int *arr3_pointer = arr3;
+  int *arr4_pointer = arr4;
+  int *temp1 = nullptr;
+
+  temp1 = arr3_pointer;
+  arr3_pointer = arr4_pointer;
+  arr4_pointer = temp1;
+
+  for (size_t i{0}; i < std::size(arr3); ++i) {
+    std::cout << arr3_pointer[i] << " ";
+  }
+  std::cout << std::endl;
+  for (size_t i{0}; i < std::size(arr4); ++i) {
+    std::cout << arr4_pointer[i] << " ";
+  }
+  std::cout << std::endl;
+
   return 0;
 }
