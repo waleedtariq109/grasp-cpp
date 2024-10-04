8f60e3ae24ad14bcafc7a9ee466ca8acfe6071b5
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-05 02:50:33 +0500

    setup

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
 int main() {
+  // Compare pointers
+
+  int scores[10]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+  int *pointer1{&scores[0]};
+  int *pointer2{&scores[8]};
+
+  std::cout << "Compairing Pointers" << std::boolalpha << std::endl;
+
+  std::cout << "pointer1 > pointer2: " << (pointer1 > pointer2) << std::endl;
+  std::cout << "pointer1 < pointer2: " << (pointer1 < pointer2) << std::endl;
+  std::cout << "pointer1 >= pointer2: " << (pointer1 >= pointer2) << std::endl;
+  std::cout << "pointer1 <= pointer2: " << (pointer1 <= pointer2) << std::endl;
+  std::cout << "pointer1 == pointer2: " << (pointer1 == pointer2) << std::endl;
+  std::cout << "pointer1 != pointer2: " << (pointer1 != pointer2) << std::endl;
+
   return 0;
 }
