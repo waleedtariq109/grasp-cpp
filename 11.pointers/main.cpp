5f409d05d4852cd25a2e2536f636713bfa24494b
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-05 02:16:36 +0500

    setup

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -1,27 +1,5 @@
 #include <iostream>
 
 int main() {
-  int scores[10]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-
-  // Array index notation
-  /*
-    int *pointer1{&scores[0]};
-    int *pointer2{&scores[8]};
-  */
-
-  // Pointer Arithmetic notation
-  int *pointer1{scores + 0};
-  int *pointer2{scores + 8};
-
-  std::cout << "pointer2 - pointer1: " << pointer2 - pointer1 << std::endl;
-  std::cout << "pointer1 - pointer2: " << pointer1 - pointer2 << std::endl;
-
-  std::ptrdiff_t positive_difference{pointer2 - pointer1};
-  std::ptrdiff_t negative_difference{pointer1 - pointer2};
-
-  std::cout << "positive_difference: " << positive_difference << std::endl;
-  std::cout << "negative_difference: " << negative_difference << std::endl;
-  std::cout << "sizeof(std::ptrdiff_t): " << sizeof(std::ptrdiff_t) << std::endl;
-
   return 0;
 }
