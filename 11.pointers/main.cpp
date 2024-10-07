78c25c71fd5241667b678ac9a2a68c7629b936fe
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-08 01:59:42 +0500

    verbose nullptr check

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -7,12 +7,17 @@ int main() {
    */
 
   int *verbosePtrCheck{nullptr};
+  verbosePtrCheck = new int(99);
 
   if (!(verbosePtrCheck == nullptr)) {
-    std::cout << "verbosePtrCheck" << verbosePtrCheck << std::endl;
+    std::cout << "verbosePtrCheck: " << verbosePtrCheck << std::endl;
+    std::cout << "*verbosePtrCheck: " << *verbosePtrCheck << std::endl;
   } else {
     std::cout << "verbosePtrCheck pointer is point to INVALID memory address" << std::endl;
   }
+
+  delete verbosePtrCheck;
+  verbosePtrCheck = nullptr;
 
   return 0;
 }
