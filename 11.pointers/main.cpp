c182a72e9389dfade230969173292f79710e1f1d
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-07 18:05:47 +0500

    uninitialized pointer

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -14,11 +14,15 @@ int main() {
    *
    * SOLUTION:
    * The solution to this is we alway initialize our pointer and point it
-   * to some value or initialize this pointer with nullptr.
+   * to some value or initialize this pointer with nullptr and if you are
+   * trying to dereferecning the pointer then make sure it point to valid
+   * memory address otherwise the program will crash or terminate.
    *
    * NOTE:
    * This is not a dangling pointer; this is just a case we might
-   * encounter if we leave our pointer uninitialized.
+   * encounter if we leave our pointer uninitialized and if we
+   * initialized the pointer to nullptr and try to dereferencing it
+   * then it will also crash the program.
    *
    */
 
@@ -26,7 +30,26 @@ int main() {
   int *uninitializedPointer;
   std::cout << "Case #1: Uninitialized pointer" << std::endl;
   std::cout << "uninitializedPointer: " << uninitializedPointer << std::endl;
-  std::cout << "*uninitializedPointer: " << *uninitializedPointer << std::endl;  // MIGHT CRASH
+  // std::cout << "*uninitializedPointer: " << *uninitializedPointer << std::endl;  // MIGHT CRASH
+
+  std::cout << std::endl;
+
+  int *uninitializedPointer1{nullptr};
+  std::cout << "Case #1: Uninitialized pointer" << std::endl;
+  std::cout << "uninitializedPointer1: " << uninitializedPointer1 << std::endl;
+  // std::cout << "*uninitializedPointer1: " << *uninitializedPointer1 << std::endl;  // CRASH
+  std::cout << std::endl;
+
+  /**
+   * *SOLUTION
+   */
+
+  int number{55};
+  int *uninitializedPointer2{&number};
+  std::cout << "Case #1: Uninitialized pointer" << std::endl;
+  std::cout << "uninitializedPointer1: " << uninitializedPointer2 << std::endl;
+  std::cout << "*uninitializedPointer1: " << *uninitializedPointer2 << std::endl;
+  std::cout << std::endl;
 
   return 0;
 }
