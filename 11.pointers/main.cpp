8d76a7d045cbe7479b9b69dcce3bc2b71a2e9bc0
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-07 18:59:26 +0500

    deleted dangling pointer

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -51,5 +51,45 @@ int main() {
   std::cout << "*uninitializedPointer1: " << *uninitializedPointer2 << std::endl;
   std::cout << std::endl;
 
+  /**
+   *
+   * Case #2: Deleted Pointer
+   *
+   * In this case when we allocate the memory on heap with the new keyword
+   * then we have to manually deallocate the memory once we deallocate the
+   * memory then our still points to the address and if we don't reinitialize
+   * the to the nullptr then our program might be crash or may lead to
+   * undefined behaviour.
+   *
+   * SOLUTION:
+   * When we release the memory and return to the system then we have to
+   * reinitialized our to the nullptr or some valid memory address because
+   * if we don't then our pointer is still pointing to that address on heap
+   * memory and to avoid this we have to reinitialized our piointer
+   *
+   */
+
+  std::cout << "Case #2: Deleted Pointer" << std::endl;
+  int *deletedPointer{new int{67}};
+
+  std::cout << "*deletedPointer - Before Deleted: " << *deletedPointer << std::endl;
+  delete deletedPointer;
+  std::cout << "*deletedPointer - After Deleted: " << *deletedPointer << std::endl;  // Still point on heap
+
+  /**
+   *
+   * * SOLUTION
+   */
+
+  int *deletedPointer1{new int{88}};
+  std::cout << "*deletedPointer1 - Before Deleted: " << *deletedPointer1 << std::endl;
+  delete deletedPointer1;
+  deletedPointer1 = nullptr;
+  // std::cout << "*deletedPointer1 - After Deleted: " << *deletedPointer1 << std::endl;  // Program will crash
+  /**
+   * The reason program will crash because we are trying to dereferenicng
+   * the nullptr
+   */
+
   return 0;
 }
