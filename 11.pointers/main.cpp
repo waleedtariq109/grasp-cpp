729271881807ccfeb6459bd2dd0802aed8cca639
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-08 01:02:25 +0500

    new keyword fails

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
 
 int main() {
+  /**
+   * The new keyword can fail if there isn't enough available memory to
+   * allocate a large array or a large block of memory. When this happens,
+   * by default, new will throw an exception.
+   *
+   * When we allocate the memory on heap which is beyond it's limit then we
+   * will get an error
+   */
+
+  // int *data = new int[1000000000000000000];  // Throw an exception
+
+  /**
+   * We can also exhaust the heap memory through loop
+   */
+
+  /*
+  for (size_t i{0}; i < 1000000000; ++i) {
+    int *data = new int[1000000000];
+  }
+  */
+
+  /**
+   * To handle this kind of behaviour we have few things
+   *
+   * 1: Exceptions
+   * 2: std::nothrow
+   */
+
+  // Exception
+  try {
+    for (size_t i{0}; i < 1000000000; ++i) {
+      int* data = new int[1000000000];
+    }
+  } catch (std::exception& err) {
+    std::cerr << "Something went wrong: " << err.what() << std::endl;
+  }
+
   return 0;
 }
