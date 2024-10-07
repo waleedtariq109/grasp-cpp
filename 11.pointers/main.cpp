6bcf6473446b692582446ca84e4c5f6326a8960a
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-08 02:48:56 +0500

    memory leaks

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
 int main() {
+  /**
+   * In C++ there is very high chance for memory leaks becasue of it's
+   * low level nature. When we allocate dynamic memory then do not
+   * deallocate properly then it may cause a memory leak
+   */
+
+  int *somePointer{new int{67}};  // Allocating dynamic memory
+
+  int number{21};
+  somePointer = &number;
+
+  /**
+   * Now the somePointer will point to number variable but the
+   * value on dynamic memory is still there but we lost access
+   * to that heap memory causing the memory leak
+   */
+
+  std::cout << "somePointer: " << *somePointer << std::endl;
+
   return 0;
 }
