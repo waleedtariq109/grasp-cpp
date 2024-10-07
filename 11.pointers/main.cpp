216b0c46ab547e060e9378a87c5c52d40ea96ae3
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-08 01:46:21 +0500

    verbose nullptr check

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
 int main() {
+  /**
+   * Verbose nullptr check is a way of checking weather the pointer
+   * is nullptr before performing any operation.
+   */
+
+  int *verbosePtrCheck{nullptr};
+
+  if (!(verbosePtrCheck == nullptr)) {
+    std::cout << "verbosePtrCheck" << verbosePtrCheck << std::endl;
+  } else {
+    std::cout << "verbosePtrCheck pointer is point to INVALID memory address" << std::endl;
+  }
+
   return 0;
 }
