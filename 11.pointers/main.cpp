57edbaa3e98a2925bff3c9462e28e5a01c9432b0
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-08 03:01:01 +0500

    memory leaks

--- 11.pointers/main.cpp
+++ 11.pointers/main.cpp
@@ -10,6 +10,7 @@ int main() {
   int *somePointer{new int{67}};  // Allocating dynamic memory
 
   int number{21};
+  // somePointer must deallocate here
   somePointer = &number;
 
   /**
@@ -19,6 +20,32 @@ int main() {
    */
 
   std::cout << "somePointer: " << *somePointer << std::endl;
+
+  /**
+   * Double Allocation
+   *
+   * If we allocate a dynamic memory and assign the address to a pointer
+   * and we again allocate a dynamic memory and assign the address to the
+   * same pointer then the previous allocated memory is still available in
+   * heap but we no longer have access to that causing the memory leak
+   *
+   */
+
+  int *allocation1{new int{76}};
+  std::cout << *allocation1 << std::endl;
+
+  // Should deallocate memory here
+
+  allocation1 = new int(94);
+  std::cout << *allocation1 << std::endl;
+
+  delete allocation1;
+  allocation1 = nullptr;
+  /**
+   * This delete statement will release the memory for the most
+   * recently allocated memory. The memory allocated initially
+   * is still present in the heap and is not freed.
+   */
 
   return 0;
 }
