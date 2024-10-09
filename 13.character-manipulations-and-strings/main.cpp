70397eb7e10b76adbe33ada679901d9cabc78fcd
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-10 02:48:37 +0500

    char manipulation

--- 13.character-manipulations-and-strings/main.cpp
+++ 13.character-manipulations-and-strings/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
 int main() {
+  // Check if character is alphanumeric
+  std::cout << std::endl;
+  std::cout << "std::isalnum" << std::endl;
+  std::cout << std::endl;
+
+  /*
+   * The std::isalnum() function checks if the given character is alphanumeric (a letter or a digit).
+   * It returns:
+   * - A non-zero integer (not necessarily 1) for true, indicating the character is alphanumeric.
+   * - 0 for false, indicating the character is not alphanumeric.
+   * In the following example:
+   * - 'C' is an alphanumeric character, so std::isalnum('C') returns 8 (a non-zero value).
+   * - '^' is not an alphanumeric character, so std::isalnum('^') returns 0.
+   */
+
+  std::cout << "C is alphanumeric: " << std::isalnum('C') << std::endl;
+  std::cout << "^ is alphanumeric: " << std::isalnum('^') << std::endl;
+
   return 0;
 }
