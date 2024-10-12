5e68f0a7488ab46a49e023b730cb62610b3c7b68
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-13 01:19:56 +0500

    std::stdcmp

--- 13.character-manipulations-and-strings/main.cpp
+++ 13.character-manipulations-and-strings/main.cpp
@@ -3,32 +3,15 @@
 
 int main() {
   /**
-   * C-String Manipulation
+   * Documented on Notion
    */
+
   const char message1[]{"The sky is blue"};
   const char *message2{"The sky is blue"};
 
   std::cout << "message1: " << message1 << std::endl;
   std::cout << "message2: " << message2 << std::endl;
   std::cout << std::endl;
-
-  /**
-   * std::strlen
-   *
-   * The strlen function expects a pointer to a C-style string (a character array)
-   * and returns the length of the string, which is the number of characters
-   * before the null terminator ('\0').
-   *
-   * The strlen function ignores the null terminator itself and does not include
-   * it in the returned length.
-   *
-   * When working with C-Strings, we need to ensure that a null terminator is
-   * present at the end of the character array. This null terminator marks the
-   * end of the string. If the null terminator is missing, the function will
-   * continue reading memory beyond the intended string, which can lead to
-   * undefined behavior until it finds a null terminator.
-   *
-   */
 
   // Character Array decay into pointer
   std::cout << "strlen(message1): " << std::strlen(message1) << std::endl;
@@ -38,25 +21,12 @@ int main() {
   std::cout << "strlen(message2): " << std::strlen(message2) << std::endl;
   std::cout << "sizeof(message2): " << sizeof(message2) << std::endl;
 
-  /**
-   * The sizeof message2 is 8 bytes because, on a 64-bit machine,
-   * the size of a pointer is typically 8 bytes.
-   *
-   * Explanation:
-   * - 1 byte = 8 bits
-   * - On a 64-bit machine, pointers are 64 bits in size.
-   * - 64 bits / 8 bits per byte = 8 bytes
-   *
-   * Therefore, sizeof(message2) returns 8 bytes because message2 is a pointer
-   * (const char*) to the string literal, and on a 64-bit system, the size
-   * of a pointer is 8 bytes.
-   */
-
   std::cout << std::endl;
 
   /**
    * std::strcmp
    */
+
   const char *stringData1{"Alabama"};
   const char *stringData2{"Blabama"};
 
@@ -65,6 +35,14 @@ int main() {
 
   // Comparing strings
   std::cout << "std::strcmp(" << stringData1 << "," << stringData2 << "): " << std::strcmp(stringData1, stringData2) << std::endl;
+  std::cout << "std::strcmp(" << stringData3 << "," << stringData4 << "): " << std::strcmp(stringData3, stringData4) << std::endl;
+
+  stringData1 = "Pakistan";
+  stringData2 = "Pakistan";
+
+  std::cout << "std::strcmp(" << stringData1 << "," << stringData2 << "): " << std::strcmp(stringData1, stringData2) << std::endl;
+
+  std::cout << std::endl;
 
   return 0;
 }
