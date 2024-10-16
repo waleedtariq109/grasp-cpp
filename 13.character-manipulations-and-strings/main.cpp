ce006370fc976bf8613c0f2e8da7dab7ea7d42d2
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-17 01:30:57 +0500

    concatenating string: + operator

--- 13.character-manipulations-and-strings/main.cpp
+++ 13.character-manipulations-and-strings/main.cpp
@@ -2,5 +2,17 @@
 #include <string>
 
 int main() {
+  std::cout << std::endl;
+
+  // Concatenating two strings: The + operator
+  std::cout << "Concatenating two strings" << std::endl;
+  std::string string1{"Hello"};
+  std::string string2{"World"};
+
+  std::string message = {string1 + " my " + string2};
+
+  std::cout << "message: " << message << std::endl;
+
+  std::cout << std::endl;
   return 0;
 }
