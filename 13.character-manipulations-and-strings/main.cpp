ee46aefda87f333c78f6a1d6e0377d8967859f79
Waleed Tariq <waleedtariq109@outlook.com>
2024-10-17 01:47:08 +0500

    concatenating string literals

--- 13.character-manipulations-and-strings/main.cpp
+++ 13.character-manipulations-and-strings/main.cpp
@@ -8,10 +8,19 @@ int main() {
   std::cout << "Concatenating two strings" << std::endl;
   std::string string1{"Hello"};
   std::string string2{"World"};
-
   std::string message = {string1 + " my " + string2};
-
   std::cout << "message: " << message << std::endl;
+  std::cout << std::endl;
+
+  // Concatenating two strings literals
+  /**
+   * This won't work and will turn into compiler error
+   * because the string literal is expanding into char
+   * array and we can't concate two array with + operator
+   */
+
+  std::cout << "Concatenating two strings literals" << std::endl;
+  // std::string string3{"Hello" + " World"}; // Compiler Error
 
   std::cout << std::endl;
   return 0;
