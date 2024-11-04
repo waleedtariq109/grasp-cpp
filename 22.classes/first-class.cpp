37b008ffb8cf87a351329e9769e611366cc0dcc3
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-04 21:10:00 +0500

    class

--- 22.classes/first-class.cpp
+++ 22.classes/first-class.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+const double PI{3.1415};
+
+class Cylinder {
+ public:
+  double baseRadius{0};
+  double height{0};
+
+  double volume() {
+    return PI * baseRadius * baseRadius * height;
+  }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  Cylinder cylinder1;
+
+  std::cout << "volume: " << cylinder1.volume() << std::endl;
+
+  cylinder1.baseRadius = 2.3;
+  cylinder1.height = 7.7;
+
+  std::cout << "volume: " << cylinder1.volume() << std::endl;
+
+  return 0;
 }
