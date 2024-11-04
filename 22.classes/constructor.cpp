0ebab6b262d1f68eace45d025296b8fa2f409fd8
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-04 21:35:56 +0500

    constructor

--- 22.classes/constructor.cpp
+++ 22.classes/constructor.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
 
+const double PI{3.1415};
+
+class Cylinder {
+ public:
+  Cylinder() {  // Defualt Constructor
+    baseRadius = 2;
+    height = 2;
+  }
+
+  Cylinder(double radiusParam, double heightParam) {
+    baseRadius = radiusParam;
+    height = heightParam;
+  }
+
+  double volume() {
+    return PI * baseRadius * baseRadius * height;
+  }
+
+ private:
+  double baseRadius{1};
+  double height{1};
+};
+
 int main() {
+  Cylinder cylinder1(3, 4);
+  std::cout << "volume: " << cylinder1.volume() << std::endl;
 }
