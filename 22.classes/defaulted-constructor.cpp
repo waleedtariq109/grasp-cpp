1ad15d5d40f8ab01e7b7f152c4e91d01f66c553c
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-04 21:55:14 +0500

    default constructor

--- 22.classes/defaulted-constructor.cpp
+++ 22.classes/defaulted-constructor.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
 
+const double PI = 3.1415;
+
+class Cylinder {
+ public:
+  Cylinder() = default;
+
+  Cylinder(double radiusParam, double heightParam) {
+    baseRadius = radiusParam;
+    height = heightParam;
+  }
+  double volume() {
+    return PI * baseRadius * baseRadius * height;
+  }
+
+ private:
+  double baseRadius{1};
+  double height{1};
+};
+
 int main() {
+  Cylinder cylinder1;
+  std::cout << "volume: " << cylinder1.volume() << std::endl;
 }
