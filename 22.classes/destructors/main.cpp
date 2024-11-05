d9e754cf9ed9976166c5a72dfcd3e8b9423a0abc
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-06 01:15:52 +0500

    destructors

---
+++ 22.classes/destructors/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string_view>
+
+class Dog {
+ public:
+  Dog() = default;
+  Dog(std::string_view nameParam, std::string_view breedParam, int ageParam);
+  ~Dog();
+
+ private:
+  std::string name;
+  std::string breedName;
+  int* agePtr{nullptr};
+};
+
+Dog::Dog(std::string_view nameParam, std::string_view breedParam, int ageParam) {
+  name = nameParam;
+  breedName = breedParam;
+  agePtr = new int{ageParam};
+  std::cout << "Constructor called for dog: " << name << std::endl;
+}
+
+Dog::~Dog() {
+  delete agePtr;
+  std::cout << "Destructor called for dog: " << name << std::endl;
+}
+
+int main() {
+  Dog myDog("Fluffy", "Husky", 3);
+}
