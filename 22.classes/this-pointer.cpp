b60520a9b5a8bc922ce94e341c61c288c0d772ee
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-12 17:23:56 +0500

    this->pointer

--- 22.classes/this-pointer.cpp
+++ 22.classes/this-pointer.cpp
@@ -6,6 +6,10 @@ class Dog {
   Dog() = default;
   Dog(std::string_view nameParam, std::string_view breedParam, int ageParam);
   ~Dog();
+  void setDogName(std::string_view name);
+  void setDogAge(int age);
+  void setDogBreed(std::string_view breed);
+  void printDogInfo();
 
  private:
   std::string name;
@@ -25,8 +29,30 @@ Dog::~Dog() {
   std::cout << "Dog destructor called for " << name << " at " << this << std::endl;
 }
 
+void Dog::setDogName(std::string_view name) {
+  this->name = name;
+}
+
+void Dog::printDogInfo() {
+  std::cout << "Dog(" << this << "): " << "[name: " << this->name << ", " << "breed: " << this->breed << ", " << "age: " << *this->age << "]" << std::endl;
+}
+
+void Dog::setDogBreed(std::string_view breed) {
+  this->breed = breed;
+}
+
+void Dog::setDogAge(int age) {
+  *this->age = age;
+}
+
 int main() {
   Dog dog1("Fluffy", "Husky", 2);
+  dog1.printDogInfo();
+
+  dog1.setDogName("Chop");
+  dog1.setDogBreed("Shephered");
+  dog1.setDogAge(3);
+  dog1.printDogInfo();
 
   std::cout << "DONE" << std::endl;
 }
