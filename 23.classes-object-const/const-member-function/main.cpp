0dd33a293f4ffe0da7ee3d405062fd37bf13c3ad
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-15 02:11:03 +0500

    setup

---
+++ 23.classes-object-const/const-member-function/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string_view>
+
+#include "dog.h"
+
+int main() {
+  std::cout << std::endl;
+
+  const Dog dog1("Chop", "Shephered", 2);
+
+  std::cout << std::endl;
+}
