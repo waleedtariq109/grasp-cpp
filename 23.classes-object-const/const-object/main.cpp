195ea0389101017ea52a2be6768a6e009904f122
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-14 18:08:29 +0500

    const obj

--- 23.classes-object-const/const-object/main.cpp
+++ 23.classes-object-const/const-object/main.cpp
@@ -4,10 +4,27 @@
 #include "dog.h"
 
 int main() {
-  Dog dog1("Chop", "Shephered", 2);
-  std::string_view name = dog1.getName();
+  /**
+   * When we make our dog object `const`, we can't read or write the
+   * data inside it. It makes sense that attempting to write data would
+   * cause a compiler error, but it may seem confusing when reading data
+   * also results in an error. After all, our intention is just to read,
+   * not to modify. So why does the compiler give an error?
+   *
+   * The answer is that the compiler doesn't distinguish between getters
+   * and setters. It's up to us to know which functions are getters and
+   * which are setters. Technically, a getter could also mutate data,
+   * so the compiler prevents any access that could lead to unintended
+   * modification. That's why it doesn't allow us to read data from a
+   * `const` object.
+   */
 
-  std::cout << "name: [getter] " << name << std::endl;
+  const Dog dog1("Chop", "Shephered", 2);
 
-  dog1.printInfo();
+  // Compiler error
+  /*
+    dog1.printInfo();
+    dog1.setName("Hassan");
+    dog1.printInfo();
+  */
 }
