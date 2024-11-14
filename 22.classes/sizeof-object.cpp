3faf651b8ddc38a3287434040c0fab47e0d2cf67
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-14 15:56:59 +0500

    sizeof object

--- 22.classes/sizeof-object.cpp
+++ 22.classes/sizeof-object.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
 
+class Wrapper {
+ public:
+  void doingSomething() {
+    std::cout << "Doing something..." << std::endl;
+  }
+
+  void printInfo() {
+    std::cout << "var1: " << this->var1 << std::endl;
+    std::cout << "var2: " << this->var2 << std::endl;
+    std::cout << "name: " << this->name << std::endl;
+  }
+
+ private:
+  int var1 = 0;
+  int var2 = 0;
+  std::string name = "your_name";
+};
+
 int main() {
+  Wrapper wrapper;
+  std::cout << "sizeof(wrapper): " << sizeof(wrapper) << std::endl;
+  wrapper.printInfo();
 }
