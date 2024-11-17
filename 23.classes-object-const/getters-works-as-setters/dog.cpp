100f7de76fe235daf6b6ba9953219b61317e38d0
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-17 18:54:27 +0500

    setup

---
+++ 23.classes-object-const/getters-works-as-setters/dog.cpp
@@ -0,0 +1,38 @@
+#include "dog.h"
+
+#include <iostream>
+
+Dog::Dog(std::string_view name, std::string_view breed, unsigned int age) {
+  this->name = name;
+  this->breed = breed;
+  this->age = age;
+};
+
+// Getters
+std::string_view Dog::getName() {
+  return this->name;
+};
+std::string_view Dog::getBreed() {
+  return this->breed;
+};
+unsigned int Dog::getAge() {
+  return this->age;
+};
+
+// Setters
+void Dog::setName(std::string_view name) {
+  this->name = name;
+};
+void Dog::setBreed(std::string_view breed) {
+  this->breed = breed;
+};
+void Dog::setAge(int age) {
+  this->age = age;
+};
+
+// Helpers
+void Dog::printInfo() const {
+  std::cout << "name: " << this->name << std::endl;
+  std::cout << "breed: " << this->breed << std::endl;
+  std::cout << "age: " << this->age << std::endl;
+};
---
+++ 23.classes-object-const/getters-works-as-setters/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_CLASS_HEADER
+#define DOG_CLASS_HEADER
+
+#include <string>
+#include <string_view>
+
+class Dog {
+ public:
+  Dog() = default;
+  Dog(std::string_view name, std::string_view breed, unsigned int age);
+
+  // Getters
+  std::string_view getName();
+  std::string_view getBreed();
+  unsigned int getAge();
+
+  // Setters
+  void setName(std::string_view name);
+  void setBreed(std::string_view breed);
+  void setAge(int age);
+
+  // Helpers
+  void printInfo() const;
+
+ private:
+  std::string_view name;
+  std::string_view breed;
+  unsigned int age;
+};
+
+#endif
