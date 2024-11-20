a87c7a51a6ec9ce57db77e2ac9157f8fa0154afd
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-21 01:36:57 +0500

    .

--- 24.deep-dive-constructor-initialization/copy-constructor/main.cpp
+++ 24.deep-dive-constructor-initialization/copy-constructor/main.cpp
@@ -3,7 +3,18 @@
 #include "person.h"
 
 int main() {
-  std::cout << std::endl;
+  Person p1("John", "Snow", 25);
+  p1.print_info();
 
-  std::cout << std::endl;
+  // Create a person copy
+  Person p2(p1);
+  p2.print_info();
+
+  std::cout << "-------" << std::endl;
+
+  p1.set_age(30);
+  p1.print_info();
+  p2.print_info();
+
+  return 0;
 }
