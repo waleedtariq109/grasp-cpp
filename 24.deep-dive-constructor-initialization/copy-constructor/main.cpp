0953ae57b07490b40d698dd0d469a2e7a2db8da2
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-24 18:32:14 +0500

    .

--- 24.deep-dive-constructor-initialization/copy-constructor/main.cpp
+++ 24.deep-dive-constructor-initialization/copy-constructor/main.cpp
@@ -3,14 +3,4 @@
 #include "person.h"
 
 int main() {
-  Person person1("Tariq", "Waleed", 23);
-  Person person2(person1);
-
-  person1.printPersonInfo();
-  person2.printPersonInfo();
-
-  person1.setPersonAge(24);
-
-  person1.printPersonInfo();
-  person2.printPersonInfo();
 }
--- 24.deep-dive-constructor-initialization/copy-constructor/person.cpp
+++ 24.deep-dive-constructor-initialization/copy-constructor/person.cpp
@@ -2,18 +2,13 @@
 
 #include <iostream>
 
-Person::Person(std::string lastName, std::string firstName, int age) :
-    lastName{lastName}, firstName{firstName}, age{new int(age)} {}
+Person::Person(std::string lastName, std::string firstName, int age) {}
 
-Person::Person(std::string lastName, std::string firstName) :
-    lastName{lastName}, firstName{firstName} {}
+Person::Person(std::string lastName, std::string firstName) {}
 
-Person::Person(std::string lastName) :
-    lastName{lastName} {}
+Person::Person(std::string lastName) {}
 
-void Person::setPersonAge(int age) const {
-  *this->age = age;
-}
+void Person::setPersonAge(int age) const {}
 
 void Person::printPersonInfo() const {
   std::cout << "Person object at " << this << " [lastName: " << this->lastName << ", firstName: " << this->firstName << ", age: " << *this->age << "]" << std::endl;
