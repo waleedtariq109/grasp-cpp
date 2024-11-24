cb23f96e84b6301805bd3d55ff565cfda6eff1b6
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-24 17:06:35 +0500

    class declaration

--- 24.deep-dive-constructor-initialization/copy-constructor/person.cpp
+++ 24.deep-dive-constructor-initialization/copy-constructor/person.cpp
@@ -1,3 +1,13 @@
 #include "person.h"
 
 #include <iostream>
+
+Person::Person(std::string lastName, std::string firstName, int age) {}
+
+Person::Person(std::string lastName, std::string firstName) {}
+
+Person::Person(std::string lastName) {}
+
+void Person::printPersonInfo() {
+  std::cout << "Person object at " << this << " [lastName: " << this->lastName << ", firstName: " << this->firstName << ", age: " << *this->age << std::endl;
+}
--- 24.deep-dive-constructor-initialization/copy-constructor/person.h
+++ 24.deep-dive-constructor-initialization/copy-constructor/person.h
@@ -1,9 +1,21 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <string>
+
 class Person {
  public:
+  Person() = default;
+  Person(std::string lastName, std::string firstName, int age);
+  Person(std::string lastName, std::string firstName);
+  Person(std::string lastName);
+
+  void printPersonInfo();
+
  private:
+  std::string firstName{};
+  std::string lastName{};
+  int* age{};
 };
 
 #endif
