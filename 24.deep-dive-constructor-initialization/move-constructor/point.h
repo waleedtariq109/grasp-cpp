6e717a0e7a7acd4efd156fef564416d5c0960bfa
Waleed Tariq <waleedtariq109@outlook.com>
2024-12-23 00:53:21 +0500

    implement class defination

--- 24.deep-dive-constructor-initialization/move-constructor/point.h
+++ 24.deep-dive-constructor-initialization/move-constructor/point.h
@@ -1,6 +1,27 @@
 #ifndef POINT_H
 #define POINT_H
 
-class Point {};
+class Point {
+  double* x{nullptr};
+  double* y{nullptr};
+
+  void invalidate() {
+    this->x = nullptr;
+    this->y = nullptr;
+  }
+
+ public:
+  Point(double x, double y);
+  Point(const Point& sourcePoint);
+  ~Point();
+
+  void setX(double x);
+  void setY(double y);
+
+  const double* getX() const;
+  const double* getY() const;
+
+  const void printInfo() const;
+};
 
 #endif
