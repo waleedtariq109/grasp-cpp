e561e9026b0949c5344f05c01b0a19d1d0083591
Waleed Tariq <waleedtariq109@outlook.com>
2024-12-28 22:10:51 +0500

    aggregate

--- 24.deep-dive-constructor-initialization/constructor-initializer-list/main.cpp
+++ 24.deep-dive-constructor-initialization/constructor-initializer-list/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 
+struct Point {
+  Point(std::initializer_list<double> list) {
+    auto it = list.begin();
+    x = *it++;
+    y = *it;
+    std::cout << "y: " << y << std::endl;
+    std::cout << "x: " << x << std::endl;
+  }
+  double x;
+  double y;
+};
+
 int main() {
+  Point p1{33.4, 55.6};
 }
