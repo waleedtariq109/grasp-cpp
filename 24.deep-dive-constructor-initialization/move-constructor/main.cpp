408563080d2978d72805f6ae1abd948d60ab252a
Waleed Tariq <waleedtariq109@outlook.com>
2024-12-28 15:20:03 +0500

    move constructor

--- 24.deep-dive-constructor-initialization/move-constructor/main.cpp
+++ 24.deep-dive-constructor-initialization/move-constructor/main.cpp
@@ -3,4 +3,6 @@
 #include "point.h"
 
 int main() {
+  Point p1(Point(33.5, 66.7));
+  std::cout << "p1.x: " << *p1.getX() << std::endl;
 }
--- 24.deep-dive-constructor-initialization/move-constructor/point.cpp
+++ 24.deep-dive-constructor-initialization/move-constructor/point.cpp
@@ -1,3 +1,20 @@
 #include "point.h"
 
 #include <iostream>
+
+Point::Point(double x, double y) : x(new double(x)), y(new double(y)) {
+  std::cout << "Constructor called" << std::endl;
+}
+
+Point::~Point() {
+  this->x = nullptr;
+  this->y = nullptr;
+}
+
+const double* Point::getX() const {
+  return this->x;
+}
+
+const double* Point::getY() const {
+  return this->y;
+}
